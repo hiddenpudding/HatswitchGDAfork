8b65a9d595afa1732b2262dae488d74786a64ac4
hiddenpudding <93959919+hiddenpudding@users.noreply.github.com>
2025-12-08 20:48:59 -0500

    exports

--- export_levels.bat
+++ export_levels.bat
@@ -1,4 +1,4 @@
 :: To add a level, add the name of the .json file (without the extension) to the list of levels
 @echo off
-py export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electromanadventures clubstep electrodynamix leveleasy dorabaebasic4 dorabaebasic7 groundtospace ultimatedestruction thenightmare rose problematic deathmoon everyend test test2
+py export_levels.py stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electromanadventures clubstep electrodynamix hexagonforce blastprocessing theoryofeverything2 retray mariometry leveleasy dorabaebasic4 spongebob groundtospace ultimatedestruction dorabaebasic7 funnygameholiday thenightmare rose problematic decode deathmoon windylandscape acropolis cataclysm stereoextremeness everyend test test2 modernmadness modcycles modfunk moddynamix randomtospace everyninja
 pause
